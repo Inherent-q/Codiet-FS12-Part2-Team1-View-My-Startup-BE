666cdf7f4f49301cbe2c6e3737ea2f61416ab1f7
Inhernet-q <tjpark941@gmail.com>
2026-04-08 11:54:22 +0900

    chore:BE,FE 라우팅 및 기본DB구조 업데이트

--- be/src/server.js
+++ be/src/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import prisma from "./client.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const corps = await prisma.corp.findMany();
+const investors = await prisma.investor.findMany();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Prisma API Server" });

6405501754f8d6001f0012c4b0fc04e993083f92
LEE SANG YUN <sensertive05@gmail.com>
2026-04-09 09:15:45 +0900

    feat: 비교 현황 페이지 초기 구현 (목 데이터)
    - 프로젝트 구조 설정 (pages/components/data 폴더)
    - Header 컴포넌트 추가 (react-router-dom 네비게이션)
    - Pagination 컴포넌트 추가
    - ComparisonStatus 페이지 구현 (정렬/페이지네이션)
    - 다크 테마 글로벌 스타일 적용 (#404040)
    - .gitignore root 통합 관리로 변경

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,15 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+import Header from './components/Header/Header';
+import ComparisonStatus from './pages/ComparisonStatus/ComparisonStatus';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/select" element={<Select />} />
-      <Route path="/compare" element={<Compare />} />
-      <Route path="/investment" element={<Investment />} />
-      <Route path="/results" element={<Results />} />
-      <Route path="/detail" element={<Detail />} />
-    </Routes>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/select" element={<Select />} />
+          <Route path="/compare" element={<ComparisonStatus />} />
+          <Route path="/investment" element={<Investment />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/detail" element={<Detail />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
@@ -21,15 +28,14 @@ function Select() {
   return <h1>나의 기업 비교 선택 페이지</h1>;
 }
 
-function Compare() {
-  return <h1>비교 현황 페이지</h1>;
-}
 function Investment() {
   return <h1>투자 현황 페이지</h1>;
 }
+
 function Results() {
   return <h1>비교 결과 페이지</h1>;
 }
+
 function Detail() {
   return <h1>기업상세 페이지</h1>;
 }
